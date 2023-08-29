da2c3763ac155b123b4f3c2c1876b813718251b4
Anaele emekah <62405019+Aneal250@users.noreply.github.com>
2023-08-29 10:07:09 +0100

    chore: navigation push

---
+++ src/views/NavView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-between">
+    <div>
+      <p>QuizAP</p>
+    </div>
+    <div>
+      <nav>
+        <router-link to="/" class="px-4">Home</router-link>
+        <router-link to="/quiz" class="px-4">Quiz</router-link>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
