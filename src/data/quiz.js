9fec3cf9d980322c9024a3ad6b945ede73432f2e
Anaele emekah <62405019+Aneal250@users.noreply.github.com>
2023-09-09 18:58:32 +0100

    chore: added quiz data

---
+++ src/data/quiz.js
@@ -0,0 +1,104 @@
+const data = [
+  {
+    id: 1,
+    quiz: [
+      {
+        question: "What is Vue?",
+        answer: 0,
+        options: ["A framework", "A library", "A type of hat"],
+        selected: null,
+      },
+      {
+        question: "What is Vuex used for?",
+        answer: 2,
+        options: [
+          "Eating a delicious snack",
+          "Viewing things",
+          "State management",
+        ],
+        selected: null,
+      },
+      {
+        question: "What is Vue Router?",
+        answer: 1,
+        options: [
+          "An ice cream maker",
+          "A routing library for Vue",
+          "Burger sauce",
+        ],
+        selected: null,
+      },
+      {
+        question: "What is a Prop",
+        answer: 2,
+        options: [
+          "A prop is a function that is reusable",
+          "A prop is a hook that is called by a parent?",
+          "Data is passed from the parent component to the child component using Component Prop.",
+        ],
+        selected: null,
+      },
+      {
+        question: "what is reactivity",
+        answer: 2,
+        options: [
+          "When you change the data value, the page is updated to reflect the changes.",
+          "React to given elements",
+          "Better way to place things together and After",
+        ],
+        selected: null,
+      },
+    ],
+  },
+  {
+    id: 2,
+    quiz: [
+      {
+        question: "What is reactJs?",
+        answer: 0,
+        options: ["A framework", "A library", "A type of hat"],
+        selected: null,
+      },
+      {
+        question: "What is Redux?",
+        answer: 2,
+        options: [
+          "Eating a delicious snack",
+          "Viewing things",
+          "State management",
+        ],
+        selected: null,
+      },
+      {
+        question: "What is Material UI?",
+        answer: 1,
+        options: [
+          "An ice cream maker",
+          "A routing library for Vue",
+          "Burger sauce",
+        ],
+        selected: null,
+      },
+      {
+        question: "What is a Prop",
+        answer: 2,
+        options: [
+          "A prop is a function that is reusable",
+          "A prop is a hook that is called by a parent?",
+          "Data is passed from the parent component to the child component using Component Prop.",
+        ],
+        selected: null,
+      },
+      {
+        question: "what are Hooks",
+        answer: 2,
+        options: [
+          "When you change the data value, the page is updated to reflect the changes.",
+          "React to given elements",
+          "Better way to place things together and After",
+        ],
+        selected: null,
+      },
+    ],
+  },
+];
