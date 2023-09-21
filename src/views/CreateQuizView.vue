66349a0f2cac6794c9287296511c62e5607a0f78
Anaele emekah <62405019+Aneal250@users.noreply.github.com>
2023-09-21 12:08:46 +0100

    chore: form inputs designs

--- src/views/CreateQuizView.vue
+++ src/views/CreateQuizView.vue
@@ -1,57 +1,90 @@
 <template>
   <form @submit.prevent="submitQuiz">
-    <label for="title">Title:</label>
-    <input type="text" id="title" v-model="quizData.title" required />
+    <div class="my-2">
+      <label for="title" class="label">Title:</label>
+      <input
+        type="text"
+        id="title"
+        v-model="quizData.title"
+        required
+        class="primary_input"
+      />
+    </div>
 
-    <label for="description">Description:</label>
-    <textarea
-      id="description"
-      v-model="quizData.description"
-      required
-    ></textarea>
+    <div class="my-2">
+      <label for="description" class="label">Description:</label>
+      <textarea
+        id="description"
+        class="primary_input"
+        v-model="quizData.description"
+        required
+      ></textarea>
+    </div>
 
     <!-- Add a button to add a new quiz question -->
-    <button type="button" @click="addQuestion">Add Question</button>
+    <button type="button" @click="addQuestion" class="secondary-btn mr-2">
+      Add Question
+    </button>
 
     <!-- Render input fields for each quiz question -->
     <div
       v-for="(question, questionIndex) in quizData.quiz"
       :key="questionIndex"
     >
-      <label>Question {{ questionIndex + 1 }}:</label>
-      <input
-        type="text"
-        :value="question.question"
-        @input="updateQuestion(questionIndex, $event)"
-        required
-      />
+      <div class="my-2">
+        <label class="label">Question {{ questionIndex + 1 }}:</label>
+        <input
+          type="text"
+          :value="question.question"
+          @input="updateQuestion(questionIndex, $event)"
+          required
+          class="primary_input"
+        />
+      </div>
 
       <!-- Add a button to add a new option for the current question -->
-      <button type="button" @click="addOption(question)">Add Option</button>
+      <button
+        type="button"
+        @click="addOption(question)"
+        class="secondary-btn mr-2"
+      >
+        Add Option
+      </button>
 
       <!-- Render input fields for options -->
       <div v-for="(option, optionIndex) in question.options" :key="optionIndex">
-        <label>Option {{ optionIndex + 1 }}:</label>
-        <input
-          type="text"
-          :value="option"
-          @input="updateOption(question, optionIndex, $event)"
-          required
-        />
+        <div class="my-2">
+          <label class="label">Option {{ optionIndex + 1 }}:</label>
+          <input
+            type="text"
+            :value="option"
+            @input="updateOption(question, optionIndex, $event)"
+            required
+            class="primary_input"
+          />
+        </div>
 
         <!-- Add a button to remove the current option -->
-        <button type="button" @click="removeOption(question, optionIndex)">
+        <button
+          type="button"
+          @click="removeOption(question, optionIndex)"
+          class="tetiary-btn"
+        >
           Remove Option
         </button>
       </div>
 
       <!-- Add a button to remove the current question -->
-      <button type="button" @click="removeQuestion(questionIndex)">
+      <button
+        type="button"
+        @click="removeQuestion(questionIndex)"
+        class="tetiary-btn"
+      >
         Remove Question
       </button>
     </div>
 
-    <button type="submit">Submit Quiz</button>
+    <button type="submit" class="primary-btn">Submit Quiz</button>
   </form>
 </template>
 
